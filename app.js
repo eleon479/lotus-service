017cf625fac930b731ac41b57a2ee666b9c050d0
eleon479 <32406542+eleon479@users.noreply.github.com>
2019-03-30 05:16:32 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require('express');
 const app = express();
 
 const { Pool } = require('pg');
-const pool = new Pool();
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 
 pool.on('error', (err, client) => {
   console.error('Unexpected error on idle client', err);
