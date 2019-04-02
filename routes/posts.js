d1e5c684e6bc0af99a0bbcb5bf1f2297eba8ff8e
eleon479 <32406542+eleon479@users.noreply.github.com>
2019-04-02 03:25:58 -0400

    added endpoint for specific posts, as well as all posts for specific users

--- routes/posts.js
+++ routes/posts.js
@@ -5,18 +5,45 @@ const { pool } = require('../services/pgstore');
 // get all posts
 router.get('/', (req, res) => {
 
-    const query = 'select posts.title, posts.contents, users.firstname, users.lastname, users.tag, users.avatar from posts join users on users.id = posts.userid;';
-    const postPromise = new Promise((resolve, reject) => {
-        pool.query(query, (err, result) => {
-            if (err || result.rows < 1) reject();
-            resolve(result.rows);
-        });
+  const userId = req.query.userId;
+  const allPostsQuery = 'SELECT posts.title, posts.contents, users.firstname, users.lastname, users.tag, users.avatar FROM posts JOIN users ON users.id = posts.userid;';
+  const userPostsQuery = `SELECT posts.title, posts.contents, users.firstname, users.lastname, users.tag, users.avatar FROM posts JOIN users ON users.id = posts.userid WHERE posts.userid = ${userId};`;
+  const query = userId ? userPostsQuery : allPostsQuery;
+  const postPromise = new Promise((resolve, reject) => {
+    pool.query(query, (err, result) => {
+      if (err || result.rows < 1) reject();
+      resolve(result.rows);
     });
+  });
 
-    postPromise
-        .then((ok) => { res.send(ok) })
-        .catch(() => { res.send([]) });
+  postPromise
+    .then(ok => {
+      res.send(ok);
+    })
+    .catch(() => {
+      res.send([]);
+    });
+});
 
+// get a specific post
+router.get('/:postId', (req, res) => {
+  
+  const postId = Number(req.params.postId);
+  const query = `SELECT posts.title, posts.contents, users.firstname, users.lastname, users.tag, users.avatar FROM posts JOIN users ON users.id = posts.userid WHERE posts.id = ${postId};`;
+  const postPromise = new Promise((resolve, reject) => {
+    pool.query(query, (err, result) => {
+      if (err) reject();
+      resolve(result.rows);
+    });
+  });
+
+  postPromise
+    .then(ok => {
+      res.send(ok[0]);
+    })
+    .catch(() => {
+      res.send({});
+    });
 });
 
 module.exports = router;
