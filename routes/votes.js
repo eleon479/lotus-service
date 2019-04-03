79be6608321451d0e42ef947639e37616bb86288
eleon479 <32406542+eleon479@users.noreply.github.com>
2019-04-03 04:46:43 -0400

    created endpoint for new votes

---
+++ routes/votes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const { pool } = require('../services/pgstore');
+
+// create a new vote
+router.post('/', (req, res) => {
+  const userId = Number(req.body.userId);
+  const postId = Number(req.body.postId);
+  const voteType = String(req.body.voteType);
+  const query = `insert into votes (userid, postid, type) values (${userId}, ${postId}, ${voteType});`;
+  const votePromise = new Promise((resolve, reject) => {
+    pool.query(query, (err, result) => {
+      if (err) reject();
+      resolve(result);
+    });
+  });
+
+  votePromise.then(res.send({ status: 'SUCCESS' })).catch(res.send({ status: 'FAILED' }));
+});
